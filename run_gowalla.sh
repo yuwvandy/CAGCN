d29c3fb732e4f22dd0389e1655e8db948c271a34
Yu Wang (Jack) <yu.wang.1@Vanderbilt.Edu>
2022-07-03 13:08:56 -0500

    Update run_gowalla.sh

--- run_gowalla.sh
+++ run_gowalla.sh
@@ -21,20 +21,20 @@ python main.py --dataset='gowalla' --model='CAGCN'  --type='sc' --trend_coeff=1 
 echo "=====CAGCN-co====="
 python main.py --dataset='gowalla' --model='CAGCN'  --type='co' --trend_coeff=1 --l2=1e-4
 
-echo "=====CAGCN-jc fusion====="
+echo "=====CAGCN*-jc====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='jc' --trend_coeff=1.2 --l2=1e-3
 
-echo "=====CAGCN-jc fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-jc - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='jc' --trend_coeff=1.2 --l2=1e-3 --embedding_dim=256
 
-echo "=====CAGCN-sc fusion====="
+echo "=====CAGCN*-sc====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='sc' --trend_coeff=1.2 --l2=1e-3
 
-echo "=====CAGCN-sc fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-sc - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='sc' --trend_coeff=1.2 --l2=1e-3 --embedding_dim=256
 
-echo "=====CAGCN-lhn fusion====="
+echo "=====CAGCN*-lhn====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='lhn' --trend_coeff=1 --l2=1e-3
 
-echo "=====CAGCN-lhn fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-lhn - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='lhn' --trend_coeff=1 --l2=1e-3 --embedding_dim=256
