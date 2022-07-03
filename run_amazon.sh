5e29648ee60d43ab5134bf4196767f76dc505295
Yu Wang (Jack) <yu.wang.1@Vanderbilt.Edu>
2022-07-03 13:09:33 -0500

    Update run_amazon.sh

--- run_amazon.sh
+++ run_amazon.sh
@@ -23,23 +23,23 @@ python main.py --dataset='amazon' --model='CAGCN' --neg_in_val_test=0 --type='lh
 
 
 
-echo "=====CAGCN-jc fusion====="
+echo "=====CAGCN*-jc====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='jc' --trend_coeff=1.7 --l2=1e-3
 
-echo "=====CAGCN-jc fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-jc - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='jc' --trend_coeff=1.7 --l2=1e-3 --embedding_dim=256
 
 
-echo "=====CAGCN-sc fusion====="
+echo "=====CAGCN*-sc====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='sc' --trend_coeff=1.7 --l2=1e-3
 
-echo "=====CAGCN-sc fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-sc - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='sc' --trend_coeff=1.7 --l2=1e-3 --embedding_dim=256
 
 
-echo "=====CAGCN-lhn fusion====="
+echo "=====CAGCN*-lhn====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='lhn' --trend_coeff=1.5 --l2=1e-3
 
 
-echo "=====CAGCN-lhn fusion - 256 (Compared with GTN)====="
+echo "=====CAGCN*-lhn - 256 (Compared with GTN)====="
 python main_fusion.py --dataset='amazon' --model='CAGCN' --type='lhn' --trend_coeff=1.5 --l2=1e-3 --embedding_dim=256
