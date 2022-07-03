b194902ea0072c6aae0b8e5eb5442f4e01649b96
Yu Wang (Jack) <yu.wang.1@Vanderbilt.Edu>
2022-07-03 10:11:30 -0500

    Update run_gowalla.sh

--- run_gowalla.sh
+++ run_gowalla.sh
@@ -1,25 +1,25 @@
 echo "=====Gowalla====="
-# echo "=====MF====="
-# python main.py --dataset='gowalla' --model='MF'
+echo "=====MF====="
+python main.py --dataset='gowalla' --model='MF'
 
-# echo "=====LightGCN====="
-# python main.py --dataset='gowalla' --model='LightGCN'
+echo "=====LightGCN====="
+python main.py --dataset='gowalla' --model='LightGCN'
 
-# echo "=====NGCF====="
-# python main.py --dataset='gowalla' --model='NGCF'
+echo "=====NGCF====="
+python main.py --dataset='gowalla' --model='NGCF'
 
 
-# echo "=====CAGCN-jc====="
-# python main.py --dataset='gowalla' --model='CAGCN'  --type='jc' --trend_coeff=1 --l2=1e-4
+echo "=====CAGCN-jc====="
+python main.py --dataset='gowalla' --model='CAGCN'  --type='jc' --trend_coeff=1 --l2=1e-4
 
-# echo "=====CAGCN-lhn====="
-# python main.py --dataset='gowalla' --model='CAGCN'  --type='lhn' --trend_coeff=1.2 --l2=1e-4
+echo "=====CAGCN-lhn====="
+python main.py --dataset='gowalla' --model='CAGCN'  --type='lhn' --trend_coeff=1.2 --l2=1e-4
 
-# echo "=====CAGCN-sc====="
-# python main.py --dataset='gowalla' --model='CAGCN'  --type='sc' --trend_coeff=1 --l2=1e-4
+echo "=====CAGCN-sc====="
+python main.py --dataset='gowalla' --model='CAGCN'  --type='sc' --trend_coeff=1 --l2=1e-4
 
-# echo "=====CAGCN-co====="
-# python main.py --dataset='gowalla' --model='CAGCN'  --type='co' --trend_coeff=1 --l2=1e-4
+echo "=====CAGCN-co====="
+python main.py --dataset='gowalla' --model='CAGCN'  --type='co' --trend_coeff=1 --l2=1e-4
 
 echo "=====CAGCN-jc fusion====="
 python main_fusion.py --dataset='gowalla' --model='CAGCN'  --type='jc' --trend_coeff=1.2 --l2=1e-3
